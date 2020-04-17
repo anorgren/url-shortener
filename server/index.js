550c72451557bccab447667ef7c8d9aa217a7da0
Adamm Norgren <norgren.adamm@gmail.com>
2020-04-17 01:48:45 -0700

    removed build

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const urlShort = require('./controllers/url.controller');
+const path = require('path');
 
-const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1/url-shortner";
+const mongoURI = process.env.MONGODB_URI || "mongodb://127.0.0.1/url-shortner";
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -21,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE");
     res.header(
         "Access-Control-Allow-Headers",
         "Content-type,Accept,x-access-token,X-Key"
@@ -33,6 +34,11 @@ app.use(function(req, res, next) {
     }
 });
 app.use('/api/urlShort', urlShort);
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server started on port`, PORT);
